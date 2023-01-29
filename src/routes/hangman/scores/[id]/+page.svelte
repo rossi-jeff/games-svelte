3f4aebe3741513e8d2623b51d6caa39f90e54142
Jeff Rossi <rossi.jeff@gmail.com>
2023-01-28 20:09:43 -0500

    sea battle details

--- src/routes/hangman/scores/[id]/+page.svelte
+++ src/routes/hangman/scores/[id]/+page.svelte
@@ -1,9 +1,31 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import type { HangMan } from '../../../../graphql/types/hang-man';
 	import { BackwardSolid } from 'svelte-awesome-icons';
+	import { graphQlClient } from '$lib';
+	import { HANG_MAN } from '../../../../graphql/queries/hang-man';
+	import { Loader } from '$lib/loader';
+	import { onMount } from 'svelte';
 
 	let id = $page.params.id;
 	console.log({ id });
+
+	let game: HangMan = {};
+
+	const getHangMan = () => {
+		Loader.set({ loading: true });
+		graphQlClient
+			.request(HANG_MAN, { id: parseInt(String(id)) })
+			.then((result) => {
+				game = result.hangMan;
+				Loader.set({ loading: false });
+			})
+			.catch((e) => console.error(e));
+	};
+
+	onMount(() => {
+		getHangMan();
+	});
 </script>
 
 <div class="back-link">
@@ -12,11 +34,59 @@
 	</a>
 </div>
 
+<div class="hang-man">
+	<div class="mb-2">
+		<strong>User</strong>
+		{game.User ? game.User.UserName : 'Anonymous'}
+	</div>
+
+	<div class="mb-2">
+		<strong>Status</strong>
+		{#if game.Status}
+			{game.Status}
+		{/if}
+	</div>
+
+	<div class="mb-2">
+		<strong>Score</strong>
+		{#if game.Score}
+			{game.Score}
+		{/if}
+	</div>
+
+	<div class="mb-2">
+		<strong>Word</strong>
+		{game.Word ? game.Word.Word : ''}
+	</div>
+
+	<div class="mb-2">
+		<strong>Length</strong>
+		{game.Word ? game.Word.Length : ''}
+	</div>
+
+	<div class="mb-2">
+		<strong>Correct</strong>
+		{#if game.Correct}
+			{game.Correct.join(', ')}
+		{/if}
+	</div>
+
+	<div class="mb-2">
+		<strong>Wrong</strong>
+		{#if game.Wrong}
+			{game.Wrong.join(', ')}
+		{/if}
+	</div>
+</div>
+
 <style>
 	div.back-link {
 		@apply mx-2 mb-4;
 	}
 	div.back-link a {
 		@apply flex;
+	}
+	div.hang-man {
+		@apply mx-2 border border-black rounded p-2;
 	}
 </style>
---
+++ src/routes/seabattle/scores/[id]/+page.svelte
@@ -0,0 +1,99 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import type { SeaBattle } from '../../../../graphql/types/sea-battle';
+	import { BackwardSolid } from 'svelte-awesome-icons';
+	import { graphQlClient } from '$lib';
+	import { SEA_BATTLE } from '../../../../graphql/queries/sea-battle';
+	import { onMount } from 'svelte';
+	import type { SeaBattleShip } from '../../../../graphql/types/sea-battle-ship';
+	import { Navy } from '../../../../graphql/types/navy';
+	import ShipDisplay from '../../ShipDisplay.svelte';
+
+	let id = $page.params.id;
+	console.log({ id });
+
+	let game: SeaBattle = {};
+	let ships: SeaBattleShip[] = [];
+	let playerShips: SeaBattleShip[] = [];
+	let opponentShips: SeaBattleShip[] = [];
+
+	const getSeaBattle = () => {
+		graphQlClient
+			.request(SEA_BATTLE, { id: parseInt(String(id)) })
+			.then((result) => {
+				game = result.seaBattle;
+				if (game.Ships) ships = game.Ships;
+				playerShips = ships.filter((s) => s.Navy === Navy.Player);
+				opponentShips = ships.filter((s) => s.Navy === Navy.Opponent);
+			})
+			.catch((e) => console.error(e));
+	};
+
+	onMount(() => {
+		getSeaBattle();
+	});
+</script>
+
+<div class="back-link">
+	<a href="/seabattle/scores">
+		<BackwardSolid class="mr-2" /> BACK
+	</a>
+</div>
+
+<div class="sea-battle">
+	<div class="mb-2">
+		<strong>User</strong>
+		{game.User ? game.User.UserName : 'Anonymous'}
+	</div>
+
+	<div class="mb-2">
+		<strong>Status</strong>
+		{#if game.Status}
+			{game.Status}
+		{/if}
+	</div>
+
+	<div class="mb-2">
+		<strong>Score</strong>
+		{#if game.Score}
+			{game.Score}
+		{/if}
+	</div>
+
+	<h2>Player Ships</h2>
+	<div class="player-ships">
+		{#if playerShips.length}
+			{#each playerShips as ship}
+				<ShipDisplay {ship} />
+			{/each}
+		{/if}
+	</div>
+
+	<h2>Opponent Ships</h2>
+	<div class="opponent-ships">
+		{#if opponentShips.length}
+			{#each opponentShips as ship}
+				<ShipDisplay {ship} />
+			{/each}
+		{/if}
+	</div>
+</div>
+
+<style>
+	div.back-link {
+		@apply mx-2 mb-4;
+	}
+	div.back-link a {
+		@apply flex;
+	}
+	div.sea-battle {
+		@apply mx-2 border border-black rounded p-2;
+	}
+	div.player-ships,
+	div.opponent-ships {
+		@apply flex flex-wrap justify-between mb-4;
+	}
+	h2 {
+		@apply text-xl font-bold;
+	}
+</style>
